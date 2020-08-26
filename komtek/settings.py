f7eab7510589ec0a92a433575bfc9bb12adc450d
afrlv1 <afrlv@yandex.ru>
2020-08-26 11:17:45 +0500

    Исправлены ошибки

--- komtek/settings.py
+++ komtek/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 1
+    'PAGE_SIZE': 10
 }
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
